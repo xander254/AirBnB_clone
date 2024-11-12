4a67504f0c40354fd354025691b279c424b36418
MauriceNgicho <mrcngicho@gmail.com>
2024-11-12 09:53:55 -0800

    base model

--- models/base_model.py
+++ models/base_model.py
@@ -6,28 +6,40 @@ import json
 import uuid
 from datetime import datetime
 
+
 class BaseModel:
     """
     Deffine the BaseModel class with common attributes and methods
     """
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
+    def __init__(self, *args, **kwargs):
+        """Initialize BaseModel instance"""
+        time_format = "%Y-%m-%dT%H:%M:%S.%f"
+        if kwargs:
+            # Loop through key-value pairs
+            for key, value in kwargs.item():
+                # Ignore class key
+                if key == "__class__":
+                    continue
+                if key in ("created_at", "updated_at"):
+                    setattr(self, key, datetime.strptime(value, time_format))
+                else:
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
 
     def __str__(self):
         """Return string representation of the instance"""
-        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+        class_name = self.__class__.__name__
+        return "[{}] ({}) {}".format(class_name, self.id, self.__dict__)
 
     def save(self):
         """Update updated_at timestamp to current time"""
         self.updated_at = datetime.now()
 
     def to_dict(self):
-        """
-        Returns a dictionary containing all keys/values
-        of __dict__ of the instance
-        """
+        """Returns a dictionary containing all keys/values of dict instance"""
          # Copy __dict__ and add __class__
         obj_dict = self.__dict__.copy()
         obj_dict['__class__'] = self.__class__.__name__
