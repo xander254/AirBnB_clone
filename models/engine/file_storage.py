eba50266c85baff36bb548b82661a73f36a62c56
MauriceNgicho <mrcngicho@gmail.com>
2024-11-14 19:32:39 -0800

    engine

---
+++ models/engine/file_storage.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""
+File storage class module.
+"""
+import json
+import os
+from models.base_model import BaseModel
+
+
+class FileStorage:
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        """Return the dictionary __objects"""
+        return FileStorage.__objects
+
+    def new(self, obj):
+        """Sets in __objects the obj with key <obj class name>.id"""
+        key = "{}.{}".format(obj.__class__.__name__, obj.id)
+        FileStorage.__objects[key] = obj
+
+    def save(self):
+        """Serializes __objects to the JSON file (__file_path)."""
+        obj_dict = {}
+        for key, obj in FileStorage.__objects.items():
+            obj_dict[key] = obj.to_dict()
+        with open(FileStorage.__file_path, "w", encoding="utf-8") as file:
+            json.dump(obj_dict, file)
+
+    def reload(self):
+        """Deserializes the JSON file to __objects if the file exists."""
+        if os.path.isfile(FileStorage.__file_path):
+            with open(FileStorage.__file_path, "r", encoding="utf-8") as f:
+                try:
+                    obj_dict = json.load(f)
+                    for key, value in obj_dict.items():
+                        cls_name = value["__class__"]
+                        if cls_name == "BaseModel":
+                            FileStorage.__objects[key] = BaseModel(**value)
+                except Exception:
+                    pass
