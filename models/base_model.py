f9b4c3824881bb32d46bcfbc5e13d4c0ef430877
xander254 <alexkinyanjui356@gmail.com>
2024-11-21 22:42:16 +0300

    Base model refactor with minor additions

--- models/base_model.py
+++ models/base_model.py
@@ -2,7 +2,6 @@
 """
 Base class Module.
 """
-import json
 import uuid
 from datetime import datetime
 import models
@@ -14,23 +13,25 @@ class BaseModel:
     """
     def __init__(self, *args, **kwargs):
         """Initialize BaseModel instance"""
-        from models import storage
         time_format = "%Y-%m-%dT%H:%M:%S.%f"
         if kwargs:
-            # Loop through key-value pairs
             for key, value in kwargs.items():
-                # Ignore class key
                 if key == "__class__":
                     continue
-                if key in ("created_at", "updated_at"):
-                    setattr(self, key, datetime.strptime(value, time_format))
+                if key == "created_at" or key == "updated_at":
+                    setattr(self, key, datetime.fromisoformat(value))
                 else:
                     setattr(self, key, value)
+            if 'id' not in kwargs:
+                self.id = str(uuid.uuid4())
+            if 'created_at' not in kwargs:
+                self.created_at = datetime.now()
+                self.updated_at = self.created_at
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = self.created_at
-            storage.new(self)
+            models.storage.new(self)
 
     def __str__(self):
         """Return string representation of the instance"""
@@ -39,16 +40,13 @@ class BaseModel:
 
     def save(self):
         """Update updated_at timestamp to current time"""
-        from models import storage
         self.updated_at = datetime.now()
-        storage.save()
+        models.storage.save()
 
     def to_dict(self):
         """Returns a dictionary containing all keys/values of dict instance"""
-        # Copy __dict__ and add __class__
         obj_dict = self.__dict__.copy()
         obj_dict['__class__'] = self.__class__.__name__
-        # Convert datetime attributes to ISO format
         obj_dict['created_at'] = self.created_at.isoformat()
         obj_dict['updated_at'] = self.updated_at.isoformat()
         return obj_dict
